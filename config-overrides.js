32589825616c8b8a4921bff5dc66f87de11268a0
tatendac <tatendac@tribetron.com>
2023-02-15 21:55:41 +0200

    updated some changes

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,12 @@
 const path = require('path');
 
+
 module.exports = function override(webpackConfig) {
   webpackConfig.module.rules.push({
     test: /\.mjs$/,
     include: /node_modules/,
     type: "javascript/auto"
   });
-  
+
   return webpackConfig;
 }
